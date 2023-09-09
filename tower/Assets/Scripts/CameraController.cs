9fba4082eecfbaa5af025ec569b26fad1d4cda98
Aaditya <aadityagupta400@gmail.com>
2023-09-09 23:35:41 +0530

    feat : clamp the position of camera

--- tower/Assets/Scripts/CameraController.cs
+++ tower/Assets/Scripts/CameraController.cs
@@ -4,48 +4,59 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     private bool doMovement = false;
 
-
     public float panSpeed = 30f;
-    public float panBorderThicknes = 10f;  // can put Screen.height/20
-
+    public float panBorderThickness = 10f;
     public float scrollSpeed = 5f;
-
     public float min_y = 10f;
     public float max_y = 80f;
-    
+    public float minX = 20f; // Minimum X coordinate
+    public float maxX = 50f;  // Maximum X coordinate
+    public float minZ = -90f; // Minimum Z coordinate
+    public float maxZ = -40f;  // Maximum Z coordinate
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             doMovement = !doMovement;
         }
 
         if (!doMovement) return;
 
-        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThicknes){
-            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        Vector3 newPosition = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            newPosition += Vector3.forward * panSpeed * Time.deltaTime;
         }
-        if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThicknes){
-            transform.Translate(-Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            newPosition -= Vector3.forward * panSpeed * Time.deltaTime;
         }
-        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThicknes){
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            newPosition += Vector3.right * panSpeed * Time.deltaTime;
         }
-        if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThicknes){
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            newPosition -= Vector3.right * panSpeed * Time.deltaTime;
         }
+
+        // Apply clamping to x and z coordinates
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        newPosition.z = Mathf.Clamp(newPosition.z, minZ, maxZ);
+
+        transform.position = newPosition;
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         Vector3 pos = transform.position;
 
-        pos.y-= scroll*1000*scrollSpeed*Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y,min_y,max_y);
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, min_y, max_y);
 
         transform.position = pos;
     }
